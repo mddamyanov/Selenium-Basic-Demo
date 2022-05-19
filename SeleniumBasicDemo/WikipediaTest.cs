158c519480ab483fd695ab8b03f792940f07e009
Mihail Damyanov <m.damyanov.d@gmail.com>
2022-05-19 17:16:58 +0300

    Upload selenium basic tests

---
+++ SeleniumBasicDemo/WikipediaTest.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// Create new Chrome browser instance
+var driver = new ChromeDriver();
+
+// Navigate to Wikipedia
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("CURRENT TITLE: " + driver.Title);
+
+// locate Search field by ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+// click on element
+searchField.Click();
+
+// fill QA and press ENTER keyboard button
+searchField.SendKeys("Quality Assurance" + Keys.Enter);
+
+
+System.Console.WriteLine("TITLE AFTER SEARCH: " + driver.Title);
+
+// Close browser
+driver.Quit();
